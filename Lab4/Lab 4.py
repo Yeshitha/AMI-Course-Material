0075d18d1bc83deb5574e8d545b40ecf80c9ede4
Yeshitha <s226587@studenti.polito.it>
2018-05-19 16:04:01 +0200

    Adding Lab4

--- Lab4/Lab 4.py
+++ Lab4/Lab 4.py
@@ -1,19 +1,69 @@
 import pymysql
 
-
+task = []
 
 if __name__ == '__main__':
+
     connection = pymysql.connect(user = 'root', password = 'root', host = 'localhost', database = 'todolist')
 
-    sql1 = "select * from todo"
+    print('Select the task which you would like to do:')
+    print('1. New Task--> 1')
+    print('2. Show Tasks--> 2')
+    print('3. Remove Tasks--> 3')
+    print('4. Remove all Tasks--> 4')
 
-    cursor = connection.cursor()
-    cursor.excute(sql1)
+    value = int(input())
 
-    result = cursor.fetchall()
+    while True:
+        if value == 1:
+            cur1 = connection.cursor()
+            print('Enter the name of the new task:')
+            new_task = str(input())
+            print('Enter a description of the task:')
+            new_description = str(input())
+            sql1 = 'INSERT to todo (id_task, description) VALUES (%s, $s)'
+            cur1.excute(sql1, (new_task,new_description))
+            connection.commit()
+            cur1.close()
 
-    for i in result:
-        print(i)
+        if value == 2:
+            cur2 = connection.cursor()
+            print('Printing the list of all the tasks...')
+            sql2 = 'SELECT * from todo'
+            cur2.excute(sql2)
+            task = cur2.fetchall()
+            if len(task) == 0:
+                print('The list is empty!')
+            else:
+                for i in task:
+                    print(task[i])
+            cur2.close()
 
-    cursor.close()
+        if value == 3:
+            cur3 = connection.cursor()
+            print('Enter the name of the task to be removed:')
+            removetask = str(input())
+            remove_task = []
+            sql3 = "SELECT * from todo"
+            cur3.excute(sql3)
+            remove_task = cur3.fetchall()
+            for i in task:
+                if i in task:
+                    remove_task.append(i)
+            for i in remove_task:
+                if i in task:
+                    task.remove(i)
+            cur3.close()
 
+        if value == 4:
+            cur4 = connection.cursor()
+            sql3 = "SELECT * from todo"
+            cur4.excute(sql3)
+            task = cur4.fetchall()
+            if len(task) == 0:
+                print('The list is already empty!')
+            else:
+                limit = len(task)
+                for i in limit:
+                    task.remove(i)
+            cur4.close()
