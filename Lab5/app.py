fcb9534ebff3e85f9cbedb4f05541e38912eaeba
Yeshitha <s226587@studenti.polito.it>
2018-05-20 02:42:48 +0200

    Adding Lab5

--- Lab5/app.py
+++ Lab5/app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect("/index.html")
+
+
+@app.route('/index.html')
+def index():
+    return render_template('/index.html')
 
 
 if __name__ == '__main__':
