662a43376a9447a467d03ac1b48e3819f28e9a90
Yeshitha <s226587@studenti.polito.it>
2018-05-20 22:44:41 +0200

    Adding Lab5, adding tasks complete, solving deleting tasks.

--- Lab5/app.py
+++ Lab5/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, redirect, render_template, request
 
 
-tasks = ['cats', 'dogs', 'birds', 'bees']
-new_task = []
-
+tasks = ['cats', 'dogs', 'birds', 'bees', 'Submit']
 
 app = Flask(__name__)
 
@@ -22,15 +20,20 @@ def index():
 def insert_task():
     if request.method == 'POST':
         task = str(request.form['task'])
-        tasks.extend(task)
+        tasks.append(task)
         return render_template('insert_task.html', value=tasks)
     return render_template('index.html', value=tasks)
 
 
-@app.route('/delete_task.html')
+@app.route('/delete_task.html', methods=['POST', 'GET'])
 def delete_task():
-
-    return render_template('delete_task.html')
+    if request.method == 'POST':
+        task = str(request.form['remove'])
+        print(task)
+        #tasks.remove(task)
+        print(tasks)
+        return render_template('delete_task.html', value=tasks)
+    return render_template('index.html', value=tasks)
 
 
 if __name__ == '__main__':
